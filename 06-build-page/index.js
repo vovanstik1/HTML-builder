e828e569f8f150007efb0e8a7b600e4add42de74
vladimir <vovanstik1@gmail.com>
2025-01-20 14:56:51 +0300

    feat: add copy assets function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,35 +2,45 @@ const fs = require('fs');
 const path = require('path');
 const dirPath = path.join(__dirname, 'assets');
 const projectDist = path.join(__dirname, 'project-dist');
+const copyPath = path.join(projectDist, 'assets');
 
 fs.mkdir(projectDist, { recursive: true }, (err) => {
   if (err) {
     console.error(`Error to create folder project-dist ${err.message}`);
+    return;
   }
 });
 
-function copyFiles() {
-  fs.mkdir(copyPath, { recursive: true }, (err) => {
+copyFiles(dirPath, copyPath);
+
+function copyFiles(src, dest) {
+  fs.mkdir(dest, { recursive: true }, (err) => {
     if (err) {
-      console.error(err.message);
+      console.error(`Error to create folder ${dest}: ${err.message}`);
       return;
     }
 
-    fs.readdir(dirPath, { withFileTypes: true }, (err, items) => {
+    fs.readdir(src, { withFileTypes: true }, (err, items) => {
       if (err) {
-        console.error(err.message);
+        console.error(`Error to read folder ${src}: ${err.message}`);
         return;
       }
 
       items.forEach((item) => {
-        const dirPathFile = path.join(dirPath, item.name);
-        const copyPathFile = path.join(copyPath, item.name);
+        const srcPath = path.join(src, item.name);
+        const destPath = path.join(dest, item.name);
 
-        fs.copyFile(dirPathFile, copyPathFile, (err) => {
-          if (err) {
-            console.error(`Error copying ${item.name}:`);
-          }
-        });
+        if (item.isDirectory()) {
+          copyFiles(srcPath, destPath);
+        } else if (item.isFile()) {
+          fs.copyFile(srcPath, destPath, (err) => {
+            if (err) {
+              console.error(
+                `Error to copy file ${srcPath} in ${destPath}: ${err.message}`,
+              );
+            }
+          });
+        }
       });
     });
   });
